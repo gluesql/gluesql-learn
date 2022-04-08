ffa6ed8a9fef80ad9c74c021a62293dce29bcdd7
Jiseok CHOI <jiseok.dev@gmail.com>
2022-04-08 18:47:26 +0900

    Implement code-editor component UI (#3)

--- components/code-editor.js
+++ components/code-editor.js
@@ -1,17 +1,37 @@
+import 'https://pagecdn.io/lib/ace/1.4.14/ace.min.js'
+
+const CDN_URL = `https://pagecdn.io/lib/ace/1.4.14/`
+
 class GlueCodeEditor extends HTMLElement {
   static get observedAttributes() { return [] }
 
   constructor() {
     super();
+    ace.config.set('basePath', CDN_URL)
 
-    this.attachShadow({ mode: 'open' });
+    const shadow = this.attachShadow({ mode: 'open' });
 
     const style = document.createElement('style');
     style.textContent = this.getStyle();
-    const h1 = document.createElement('h1');
-    h1.textContent = 'GlueCodeEditor';
 
-    this.shadowRoot.append(style, h1);
+    const dom = require("ace/lib/dom");
+    dom.buildDom([
+      ["div", {id: "sql-editor"}],
+      ["style", this.getEditorStyle()]
+    ], shadow)
+
+    const sqlEditor = ace.edit(this.shadowRoot.querySelector('#sql-editor'), {
+      mode: "ace/mode/sql",
+      value: "CREATE TABLE ITEM (id Integer)",
+      autoScrollEditorIntoView: true,
+    })
+
+    sqlEditor.on('input', () => {
+      console.log(this)
+    })
+
+    this.sqlEditor = sqlEditor
+    sqlEditor.renderer.attachToShadowRoot();
   }
 
   connectedCallback() {
@@ -22,12 +42,17 @@ class GlueCodeEditor extends HTMLElement {
 
   getStyle() {
     return `
-      :host {
-        display: inline-block;
-
-        background-color: #ea19ca;
-      }
+      :host {}
     `;
+  }
+
+  getEditorStyle() {
+    return `
+      #sql-editor {
+        grid-area: html;
+        height: 100%;
+      }
+  `;
   }
 }
 
