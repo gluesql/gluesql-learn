2f5e585af42d9d172142d39226c2be10c71c0aa8
Taehoon Moon <taehoon.moon@outlook.com>
2022-04-08 18:41:27 +0900

    Implement <quiz-list> component UI (#1)

--- components/quiz-list.js
+++ components/quiz-list.js
@@ -8,10 +8,10 @@ class GlueQuizList extends HTMLElement {
 
     const style = document.createElement('style');
     style.textContent = this.getStyle();
-    const h1 = document.createElement('h1');
-    h1.textContent = 'GlueQuizList';
 
-    this.shadowRoot.append(style, h1);
+    const aside = document.createElement('aside');
+    aside.innerHTML = this.update();
+    this.shadowRoot.append(style, aside);
   }
 
   connectedCallback() {
@@ -20,14 +20,87 @@ class GlueQuizList extends HTMLElement {
   attributeChangedCallback() {
   }
 
+  update() {
+    const basics = [
+      'Hello world',
+      'Data types',
+      'SELECT 1',
+      'SELECT 2',
+    ];
+
+    const advanced = [
+      'More stuffs',
+      'Something more',
+      'Harder',
+      'Quite Harder',
+    ];
+
+    return `
+      ${this.renderSection('Basics', basics)}
+      ${this.renderSection('Advanced', advanced)}
+    `;
+  }
+
+  renderSection(category, quizList) {
+    return `
+      <section>
+        <h3>${category}</h3>
+        <ul>
+          ${quizList.map(name => (
+            `<li><button>${name}</button></li>`
+          )).join('')}
+        </ul>
+      </section>
+    `;
+  }
+
   getStyle() {
     return `
       :host {
         display: inline-block;
 
-        background-color: red;
+        background-color: white;
 
-        font-family: 'Roboto Mono', monospace;
+        --border-color: #ddd;
+
+        overflow-x: hidden;
+        overflow-y: auto;
+      }
+
+      aside {
+        width: 100%;
+        padding: 10px;
+        border-right: 1px solid var(--border-color);
+      }
+
+      h3 {
+        margin-top: 20px;
+        margin-bottom: 10px;
+      }
+
+      ul {
+        margin-top: 10px;
+        padding-inline-start: 30px;
+      }
+
+      button {
+        background-color: transparent;
+        border: none;
+        box-shadow: none;
+
+        font-family: inherit;
+        font-size: 16px;
+        border-radius: 4px;
+
+        cursor: pointer;
+      }
+
+      button:hover {
+        background-color: rgba(100, 100, 100, 0.2);
+      }
+
+      button:active {
+        background-color: rgba(100, 100, 100, 0.3);
       }
     `;
   }
