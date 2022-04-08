e59afc8f77da3df79fcc4631533164d186dcf29e
Jiseok CHOI <jiseok.dev@gmail.com>
2022-04-08 21:20:28 +0900

    Implement <table-viewer> Component UI

--- components/table-viewer.js
+++ components/table-viewer.js
@@ -8,10 +8,21 @@ class GlueTableViewer extends HTMLElement {
 
     const style = document.createElement('style');
     style.textContent = this.getStyle();
-    const h1 = document.createElement('h1');
-    h1.textContent = 'GlueTableViewer';
 
-    this.shadowRoot.append(style, h1);
+    const table = document.createElement('table')
+    table.innerHTML = `
+      ${this.renderTableHeader([
+        'Name',
+        'Age',
+        'Temp'
+      ])}
+      ${this.renderTableBody([
+        ['1', '2', '3'],
+        ['4', '5', '6'],
+      ])}
+    `
+
+    this.shadowRoot.append(style, table);
   }
 
   connectedCallback() {
@@ -20,12 +31,56 @@ class GlueTableViewer extends HTMLElement {
   attributeChangedCallback() {
   }
 
+  renderTableHeader(headers) {
+    return `
+      <thead>
+        <tr>
+          ${headers.map(header => `<th>${header}</th>`).join('')}
+        </tr>
+      </thead>
+    `
+  }
+
+  renderTableBody(rows) {
+    return `
+      <tbody>
+        ${rows.map(row => `<tr>${row.map(data => `<td>${data}</td>`).join('')}</tr>`).join('')}
+      </tbody>
+    `;
+  }
+
   getStyle() {
     return `
       :host {
         display: inline-block;
+      }
 
-        background-color: #19a1ca;
+      table {
+        margin: 6px 12px;
+        width: calc(100% - 24px);
+        overflow: hidden;
+        box-sizing: border-box;
+        border-collapse: collapse;
+      }
+
+      td, th {
+        width: 14.29%;
+        height: 34px;
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 20px;
+
+        border: 1px solid #172334;
+      }
+
+      th {
+        color: #6B7280;
+      }
+
+      td {
+        text-align: center;
+
+        color: #111827;
       }
     `;
   }
