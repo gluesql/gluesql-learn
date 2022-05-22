c0d95a018c7632b426524ca44d953d76459d62c9
Kim-Yong-Soo <dydtnalswl@naver.com>
2022-05-22 10:47:10 +0900

    ADD INTEGER QUIZ

--- quiz/data-types/integer.js
+++ quiz/data-types/integer.js
@@ -1,16 +1,26 @@
 const name = "INTEGER"
 const category = "Data type";
 const schemaSQL = `
-  CREATE TABLE Item (
+  CREATE TABLE User (
     id INTEGER,
-    name TEXT
+    name TEXT,
+  );
+  CREATE TABLE BankBook (
+    user_id INTEGER,
+    price INTEGER,
+    type TEXT,
+    checked INTEGER,
   );
 `;
+
 const dataSQL = `
-  INSERT INTO Item VALUES (1, "Foo"), (2, "Bar"), (3, "Coz");
+  INSERT INTO User VALUES (1, "Foo"), (2, "Bar"), (3, "Coz");
+  INSERT INTO BankBook VALUES (1, 100000, "deposit", 1), (1, -10000, "withdraw", 1), (1, -50000, "withdraw", 1), (1, -990000, "payAfter", 1), (1, 500000, "deposit", 1),
+  (2, -490000, "payAfter", 1), (2, 100000, "deposit", 0), (2, 200000, "deposit", 1),
+  (3, 500000, "deposit", 1), (3, -200000, "withdraw", 0), (3, -100000, "payAfter", 1);
 `;
 const solutionSQL = `
-  SELECT id, name FROM Item;
+  SELECT User.name, SUM(price) FROM BankBook JOIN User ON User.id = BankBook.user_id GROUP BY user_id;
 `;
 
 const quiz = `
