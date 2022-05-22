612ecfe3dce3ebff08ee114d9c84769a266f6166
Hyewon Lyu <52669844+lyuhw1023@users.noreply.github.com>
2022-05-22 11:17:58 +0900

    Add float quiz with limit (#34)

--- quiz/data-types/float.js
+++ quiz/data-types/float.js
@@ -1,17 +1,18 @@
 const name = "FLOAT"
 const category = "Data type";
 const schemaSQL = `
-  CREATE TABLE Item (
-    id INTEGER,
-    name TEXT
+  CREATE TABLE student (
+    name TEXT,
+    score FLOAT
   );
 `;
 const dataSQL = `
-  INSERT INTO Item VALUES (1, "Foo"), (2, "Bar"), (3, "Coz");
+  INSERT INTO student VALUES ("seoyeon", 80.5), ("jiwon", 90.2), ("hyewon", 95.2), ("jisoo", 78.2),("jimin",89.5);
 `;
 const solutionSQL = `
-  SELECT id, name FROM Item;
+  SELECT score, name FROM student ORDER BY score DESC LIMIT 3;
 `;
+
 
 const quiz = `
   INSERT INTO Quiz VALUES (
